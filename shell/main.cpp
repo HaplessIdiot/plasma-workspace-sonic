7d465903071f876d0c9b2e5d6a60cd6e9aededab
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-10-31 11:17:43 +0200

    shell: Set M_TRIM_THRESHOLD
    Copied from kwin. Its main purpose is to ensure that memory usage stays
    at a reasonable level after loading and dropping a wallpaper, that was
    loaded in a worker thread.

--- shell/main.cpp
+++ shell/main.cpp
@@ -37,8 +37,31 @@
 
 #include <csignal>
 
+#if __has_include(<malloc.h>)
+#include <malloc.h>
+#include <unistd.h>
+#endif
+
+#if __has_include(<malloc.h>)
+static void setupMalloc()
+{
+    // Prevent fragmentation of the heap by malloc (glibc).
+    //
+    // The default threshold is 128*1024, which can result in a large memory usage
+    // due to fragmentation especially if we use the raster graphicssystem. On the
+    // otherside if the threshold is too low, free() starts to permanently ask the kernel
+    // about shrinking the heap.
+    const int pagesize = sysconf(_SC_PAGESIZE);
+    mallopt(M_TRIM_THRESHOLD, 5 * pagesize);
+}
+#endif
+
 int main(int argc, char *argv[])
 {
+#if __has_include(<malloc.h>)
+    setupMalloc();
+#endif
+
 #if QT_CONFIG(qml_debug)
     if (qEnvironmentVariableIsSet("PLASMA_ENABLE_QML_DEBUG")) {
         QQmlDebuggingEnabler::enableDebugging(true);
